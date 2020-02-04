54eda1d6a041e7d3f4a5041d679cb3bd3b989bb0
Randy Steele <steelerx155@gmail.com>
2020-02-04 01:34:11 +0000

    removed corneal created welcome pge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,13 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-
-  configure do
+  
+ configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :session_secret, "password_security"
   end
 
-  get "/" do
-    erb :welcome
-  end
-  
   get "/home" do 
     erb :home 
   end
