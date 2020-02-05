74b10ea4a549b4cdf8551ee180eb7a5415299c8a
Randy Steele <steelerx155@gmail.com>
2020-02-05 21:00:46 +0000

    deleted and reran users table migration due to error when running migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200204012538) do
+ActiveRecord::Schema.define(version: 20200205205814) do
 
   create_table "destinations", force: :cascade do |t|
     t.string  "city"
