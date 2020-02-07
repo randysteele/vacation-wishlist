ab546c8646a09253395efee45bb0e9291c95b2be
Randy Steele <steelerx155@gmail.com>
2020-02-07 23:40:21 +0000

    changed routes to remove the users nest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,26 @@
 class UsersController < ApplicationController 
   
   
-  get '/users/login' do 
+  get '/signup' do 
+      
+    erb :'users/signup'
+  end
+  
+  post '/signup' do
+    if params[:username] == "" || params[:password] == ""
+      redirect '/failure'
+    else
+      User.create(username: params[:username], password: params[:password])
+      redirect '/login'
+    end
+end
+  
+  
+  get '/login' do 
     erb :'/users/login'
   end
   
-  post '/users/login' do 
+  post '/login' do 
     @user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = @user.id
@@ -16,33 +31,14 @@ class UsersController < ApplicationController
   end
   
   
-  
-   get '/users/signup' do 
-      
-    erb :'/users/signup'
-  end
-  
-  get '/users/:id' do
+  get '/edit' do
     @user = User.find_by_id(params[:id])
-    erb :'/users/edit'
+    erb :edit
   end  
     
    
-  
-  post "/signup" do
-    if params[:username] == "" || params[:password] == ""
-      redirect '/failure'
-    else
-      User.create(username: params[:username], password: params[:password])
-      redirect '/login'
-    end
-end
 
-  #   post "/signup" do
-  # if params[:username] == "" || params[:password] == ""
-  # redirect '/index'
-  # end
-
+ 
   get "/home" do 
     erb :home 
   end
