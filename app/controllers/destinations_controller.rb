28f7c2382f244296997043931702fd73342dcf01
Randy Steele <steelerx155@gmail.com>
2020-02-07 16:16:39 +0000

    added routes to destination controller

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,8 +1,35 @@
 class DestinationController > Sinatra::Base 
   
+  get '/destinations' do 
+    if logged_in?
+      @destinations = Destination.all
+      erb :'destinations'
+    end
+    
+    get '/destinations/new' do
+    if logged_in?
+      erb :'destinations/create_destination'
+    else
+      redirect to '/login'
+    end
+  end
   
-  
-  
+  post '/destinations' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to "/destinations/new"
+      else
+        @tweet = current_user.tweets.build(content: params[:content])
+        if @destination.save
+          redirect to "/destinations/#{@destinations.id}"
+        else
+          redirect to "/destinations/new"
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end
 
 
 
