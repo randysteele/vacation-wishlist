cc26d1f26610435bb51959e29a1b5d32a0f67990
Randy Steele <steelerx155@gmail.com>
2020-02-10 20:58:49 +0000

    remove error in show page

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,61 +1,35 @@
 class DestinationsController < ApplicationController
   
-  # get '/destinations/show' do 
-  #   @destination = Destination.all 
-  #     erb '/destinations/show'
-  #   end
+  get '/destinations/show' do 
+    @destination = Destination.all 
+      erb '/destinations/show'
+    end
     
    
   get '/destinations/new' do 
     erb :'/destinations/new'
   end
-  
+
   post '/destinations/new' do
     if logged_in?
-      @destinations = Destination.all 
+      @destinations = Destinations.all 
+     
       erb :'/destinations/new'
     else 
-      redirect to '/destinations/show'
-     end
-  end
-  
-  
-  get '/destinations/:id' do 
-    if logged_in?
-      @destinations = Destination.find_by_id(params[:id])
-      erb :'/destinations/show'
-    else 
-      # redirect to '/destinations/show'
-    end
-  end
-  
-  
-  get '/destinations/:show' do
-    if logged_in?
-      @destinations = Destination.find_by_id(params[:id])
-      erb :'/destinations/show'
-  end
- end
- 
-  post '/destinations/:show' do 
+       redirect to '/login'
+end
+end
+
+
+  get "/destinations/show" do
     @destinations = Destination.all
+    @destinations = Destination.find_by(params[:id])
     erb :'/destinations/show'
   end
-  
-  patch '/destinatinos/show' do 
-    if logged_in?
-      if params [:city] == ""
-        redirect to "/destinations/#{params[:id]}/edit"
-  #     else 
-  #       @destinations = Destination.find_by_id(params[:id])
-  #       if @destinations.update(city: parmas[:city])
-  #         redirect to '/destinations/#{@destinations.id}/edit'
-         end
-  #     elsif 
-  #       redirect to '/destinations/show'
-       end
-     end
-  # elsif
-  #   redirect to '/login'
 
+  post '/destinations/show' do 
+   @destinations = "#{params[:city]}. #{params[:state]}"
+   
+    erb :'/destinations/show'
+  end
 end
