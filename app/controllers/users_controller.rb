8f6fd936f3bcf192e99f183fffe0f3d7c636208a
Randy Steele <steelerx155@gmail.com>
2020-02-11 21:50:42 +0000

    correcting login routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,23 +7,30 @@ class UsersController < ApplicationController
   
   post '/login' do 
     binding.pry
+    @user = User.find_by(:email => params[:email])
+    if @user.authenticate(params[:password])
+    session[:user_id] = @user.id    
     
+    else   
+      
+      
   end
+end
   
   
   get '/signup' do 
     erb :signup
   end
   
-  post '/login' do 
-     user = User.find_by(:email => params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect to "/destinations/show"
-    else
-      # redirect to '/signup'
-     end
-  end
+  # post '/login' do 
+  #   user = User.find_by(:email => params[:username])
+  #   if user && user.authenticate(params[:password])
+  #     session[:user_id] = user.id
+  #     redirect to "/destinations/show"
+  #   else
+  #     # redirect to '/signup'
+  #   end
+  # end
   
   
   get '/edit' do
