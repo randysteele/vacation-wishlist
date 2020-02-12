5f651ca6c662b7327e5b64a7d948eb83e888c52e
Randy Steele <steelerx155@gmail.com>
2020-02-12 01:10:14 +0000

    completed users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,47 +16,35 @@ class UsersController < ApplicationController
   end
 end
 
+get '/signup' do
+    redirect_if_logged_in
+    erb :signup
+  end
+
    post '/users' do 
-   if params[:name] != "" && params[:email] != "" && params[:password] != ""
-   @user = User.create[params]
-    redirect to '/users/#{user.id}'
-   else
-    flash[:errors] = "Your username or password is invalid.  Please sign up or try again."
+     @user = User.new(params)
+     if @user.save 
+       session[:user_id] = @user.id
+   flash[:message] = "You have successfully created an account, #{@user.name}! Welcome!"
+      redirect "/users/#{@user.id}"
+    else
+      flash[:errors] = "Account creation failure: #{@user.errors.full_messages.to_sentence}"
+      redirect '/signup'
   end
 end 
   
   get '/users/:id' do
-    erb :'/users/show' 
+    @user = User.find_by(id: params[:id])
+    redirect_if_not_logged_in
+    erb :'/users/show'
+   end
   end
-  
-  
-  get '/signup' do 
-    erb :signup
-  end
-  
-  # post '/login' do 
-  #   user = User.find_by(:email => params[:username])
-  #   if user && user.authenticate(params[:password])
-  #     session[:user_id] = user.id
-  #     redirect to "/destinations/show"
-  #   else
-  #     # redirect to '/signup'
-  #   end
-  # end
-  
-  
-  get '/edit' do
-    @user = User.find_by_id(params[:id])
-    erb :edit
-  end  
 
  
    get '/logout' do
     if logged_in?
-      session.destroy
-      redirect to '/login'
-    else
-      redirect to '/'
+      session.clear
+      redirect '/'
     end
   end
 end
