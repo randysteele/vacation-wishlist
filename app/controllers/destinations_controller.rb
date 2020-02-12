1f34f76caa3bd2165a660d73a6afdd652dc913aa
Randy Steele <steelerx155@gmail.com>
2020-02-12 15:08:23 +0000

    building controller, destinations route

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,17 +1,23 @@
 class DestinationsController < ApplicationController
   
-  # get '/destinations/show' do 
-  #   @destination = Destination.all 
-  #     erb '/destinations/show'
-  #   end
+  get '/destinations' do 
+    @destination = Destination.all 
+      erb '/destinations/index'
+    end
     
    
   get '/destinations/new' do 
+    redirect_if_not_logged_in
     erb :'/destinations/new'
   end
 
-  post '/destinations/new' do
-      @destinations = Destination.all 
+  post '/destinations/' do
+    if params[:city] != ""
+      @destinations = Destination.create(:city params[:city], :user_id current_user.id, :state params[:state], :distance params[:distance])
+      flash[:message] = "Congrats! You've Successfully Created A New Destination!" if @destinations.id
+      redirect "/destinations/#{@destinations.id}"
+    else 
+      flash[:message] "Sorry, that didn't work! All fields are required!"
        erb :'/destinations/new'
 end
 
