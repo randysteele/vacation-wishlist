06d37ce9cede3775154079619dc83ec1b935b7ce
Randy Steele <steelerx155@gmail.com>
2020-02-14 03:13:13 +0000

    updated delete method so it operates properly

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -6,7 +6,7 @@ class DestinationsController < ApplicationController
       erb :'/destinations/index'
     end
     
-    post '/destinations/:id' do
+    post '/destinations/' do
     # redirect_if_not_logged_in
     if params[:city] != ""
      @destinations = Destination.create(:city => params[:city], :user_id => current_user.id, :state => params[:state], :distance => params[:distance])
@@ -19,20 +19,7 @@ class DestinationsController < ApplicationController
  end
 end
 
-#   post '/destinations/' do
-#     # redirect_if_not_logged_in
-#     if params[:city] != ""
-#     @destinations = Destination.create(:city => params[:city], :user_id => current_user.id, :state => params[:state], :distance => params[:distance])
-#   # @destinations = Destination.create(city: params[:city], user_id: current_user.id, state: params[:state], distance: params[:distance]) 
-#       flash[:message] = "Congrats! You've Successfully Created A New Destination!" if @destinations.id
-#       redirect "/destinations/#{@destinations.id}"
-#     else 
-#       flash[:message] = "Sorry, that didn't work! All fields are required!"
-#       redirect'/destinations/new'
-# end
-# end
-    
-   
+
   get '/destinations/new' do 
    redirect_if_not_logged_in
     erb :'/destinations/new'
@@ -47,13 +34,13 @@ end
   
   delete '/destinations/:id' do 
     @destinations = Destination.find_by_id(params[:id])
-    if authorized_to_edit?(@destinations)
+    # if authorized_to_edit?(@destinations)
       @destinations.destroy
       flash[:message] = "Successfully deleted the selected destination"
       redirect '/destinations'
-      else
-        redirect '/destinations'
-      end
+    #  else
+     #   redirect '/destinations'
+     # end
     end
     
     patch '/destinations/:id' do 
